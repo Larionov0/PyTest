f1d57bb3b0f74d9c51e62540c684f6a065fdccf9
Larko <larionov1001@gmail.com>
2019-10-23 23:01:07 +0300

    First views with lol kek cheburek

---
+++ catalog/models.py
@@ -0,0 +1,59 @@
+from django.db import models
+from django.urls import reverse
+from json import dumps, loads
+
+
+# Create your models here.
+class Pack(models.Model):
+    """
+    Це типу пак (набір) з тестами.
+    Має містити список питань
+    """
+    name = models.CharField(max_length=40, help_text="Name of pack")
+    questions_json = models.TextField(help_text="List of questions id-s converted to json")
+    reward = models.IntegerField(help_text="Reward in Paisons")
+
+    def __str__(self):
+        return str(self.name) + " pack"
+
+
+class Question(models.Model):
+    """
+    А це типу саме питання.
+    Містить питання, список (як бінарник..) відповідей, індекс правильної.
+    """
+    def __init__(self, *args, **kwargs):
+        answers = kwargs.pop("answers")
+        super().__init__(*args, **kwargs)
+        self.set_answers(answers)
+
+    question = models.CharField(max_length=1000, help_text="Question`s text")
+    answers_json = models.TextField(help_text="List of answers converted to json")
+    index_of_correct = models.IntegerField(help_text="Index of correct answer")
+
+
+    # Metadata
+    class Meta:
+        pass
+
+    def set_answers(self, lst):
+        self.answers = dumps(lst)
+
+    def get_answers(self):
+        return loads(self.answers)
+
+    def get_absolute_url(self):
+        """
+        Returns the url to access a particular instance of MyModelName.
+        """
+        return reverse('model-detail-view', args=[str(self.id)])
+
+    def __str__(self):
+        """
+        String for representing the MyModelName object (in Admin site etc.)
+        """
+        return f"{self.question}"
+
+
+first_question = Question(question="WTF?", answers=[1, 2, 3], index_of_correct=1)
+print(first_question)
