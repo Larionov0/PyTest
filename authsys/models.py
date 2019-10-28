4278d2bc845724a040b772fc85674fca5d4dbe93
Larko <larionov1001@gmail.com>
2019-10-28 13:29:04 +0200

    Added UserProfile

--- authsys/models.py
+++ authsys/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+from catalog.models import Pack
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    completed_packs = models.ManyToManyField(Pack)
+    paisons = models.IntegerField(default=0)
